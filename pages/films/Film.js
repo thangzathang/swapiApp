f257aa97bdf7725b960e0fdf893504d6637f6d93
thangzathang <thangzathang@gmail.com>
2021-12-22 14:34:37 +1100

    Just the title

--- pages/films/Film.js
+++ pages/films/Film.js
@@ -4,7 +4,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-function Film({ index }) {
+function Film({ film, index }) {
   //   const date = film.release_date.split("-");
   //   const formattedDate = date[2] + "/" + date[1] + "/" + date[0];
 
@@ -13,6 +13,7 @@ function Film({ index }) {
   return (
     <div className="filmContainer">
       <h1>{index}</h1>
+      <h1>{film.title}</h1>
       {/* <Image src={`/poster${film.episode_id}.jpg`} width={350} height={500} alt={`Star wars poster for ep ${film.episode_id}`} /> */}
       {/* <h1>
         <Link href={`/films/` + film.url.split("/")[5]} key={film.url.split("/")[5]}>
