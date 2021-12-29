97c1275fe123435a5d4e96b5e837b427e246423f
thangzathang <thangzathang@gmail.com>
2021-12-29 15:13:29 +1100

    background card revamp and film styled better

--- pages/films/index.js
+++ pages/films/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import Film from "./Film";
 import Image from "next/image";
+import React, { useState } from "react";
 
 // This function will run at build time.
 export const getStaticProps = async () => {
@@ -18,6 +19,12 @@ export const getStaticProps = async () => {
 };
 
 const Films = ({ films }) => {
+  const [filteredFilms, setFilteredFilms] = useState("");
+
+  let day = 0;
+  let month = 0;
+  let year = 0;
+
   return (
     <div>
       <div className="searchInput">
@@ -31,13 +38,21 @@ const Films = ({ films }) => {
               <Image src={`/poster${film.episode_id}.jpg`} width={350} height={500} alt={`Star wars poster for ep ${film.episode_id}`} />
             </div>
             <h1>
-              <Link href={`/films/` + film.url.split("/")[5]} key={film.url.split("/")[5]} className="primary">
+              <Link href={`/films/` + film.url.split("/")[5]} key={film.url.split("/")[5]}>
                 {film.title}
               </Link>
             </h1>
             <div className="filmContent">
-              <h2>Directed By: {film.director}</h2>
-              <h3>Release Date: {film.release_date}</h3>
+              <h2>Director: {film.director}</h2>
+              <div className="noDisplay">
+                {(year = film.release_date.split("-")[0])}
+                {(month = film.release_date.split("-")[1])}
+                {(day = film.release_date.split("-")[2])}
+              </div>
+
+              <h3>
+                Release Date: {day}/{month}/{year}
+              </h3>
             </div>
           </div>
         ))}
